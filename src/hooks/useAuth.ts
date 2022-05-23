9d6f245145cb8b4e04fd729a02b67f2c7356a3d7
ryo-hidaka <66785120+ryo-hidaka@users.noreply.github.com>
2022-05-23 18:35:57 +0900

    ログイン時のトースト作成

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,28 +1,35 @@
-/* eslint-disable  react-hooks/exhaustive-deps */
-
 import axios from "axios";
 import { useCallback, useState } from "react";
 import { useHistory } from "react-router-dom";
+
 import { User } from "../types/user";
+import { useMessage } from "./useMessage";
 
 export const useAuth = () => {
   const history = useHistory();
+  const { showMessage } = useMessage();
   const [loading, setLoading] = useState(false);
-  const login = useCallback((id: string) => {
-    setLoading(true);
-    axios
-      .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
-      .then((res) => {
-        if (res.data) {
-          history.push("/home");
-        } else {
-          alert("ユーザが見つかりません");
-        }
-      })
-      .catch(() => alert("ログインできません"))
-      .finally(() => {
-        setLoading(false);
-      });
-  }, []);
+  const login = useCallback(
+    (id: string) => {
+      setLoading(true);
+      axios
+        .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
+        .then((res) => {
+          if (res.data) {
+            showMessage({ title: "ログインしました", status: "success" });
+            history.push("/home");
+          } else {
+            showMessage({ title: "ユーザーが見つかりません", status: "error" });
+          }
+        })
+        .catch(() =>
+          showMessage({ title: "ログインできません", status: "error" })
+        )
+        .finally(() => {
+          setLoading(false);
+        });
+    },
+    [history, showMessage]
+  );
   return { login, loading };
 };
